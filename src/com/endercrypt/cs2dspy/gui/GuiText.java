f554d24de687a3e6b4ad4d1f0421684c8cb603dd
Magnus <EnderCrypt@gmail.com>
2017-01-21 01:57:51 +0100

    added missing copyright header on GuiText.java

--- src/com/endercrypt/cs2dspy/gui/GuiText.java
+++ src/com/endercrypt/cs2dspy/gui/GuiText.java
@@ -6,6 +6,24 @@ import java.awt.Graphics2D;
 import java.util.ArrayDeque;
 import java.util.Deque;
 
+/**
+ *	This file is part of Cs2dSpy and was created by Magnus Gunnarsson
+ *
+ *	Copyright (C) 2017  Magnus Gunnarsson (EnderCrypt)
+ *
+ *	This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 public class GuiText
 {
 	private static final int PADDING_WIDTH = 2;
