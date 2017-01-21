10367d8dc32d21c74a13ea881805c065f599422d
Magnus <EnderCrypt@gmail.com>
2017-01-21 19:12:37 +0100

    made lua error report properly if file path is wrong

--- cs2d/sys/lua/Cs2dSpy/UtilityFunctions.lua
+++ cs2d/sys/lua/Cs2dSpy/UtilityFunctions.lua
@@ -2,8 +2,17 @@ function Cs2dSpy.print(text)
 	print("[Cs2dSpy] "..text)
 end
 
+function Cs2dSpy.error(text)
+	error("[Cs2dSpy] "..text)
+end
+
 function Cs2dSpy.openLink(fileName)
-	return io.open(Cs2dSpy.linkDirectory..fileName.."."..Cs2dSpy.linkFileType, "w")
+	local fullFileName = Cs2dSpy.linkDirectory..fileName.."."..Cs2dSpy.linkFileType
+	local file = io.open(fullFileName, "w")
+	if (file == nil) then
+		Cs2dSpy.error("Failed to open link (file: "..fullFileName..")")
+	end
+	return file
 end
 
 function Cs2dSpy.cs2dWeaponTypeCount()
