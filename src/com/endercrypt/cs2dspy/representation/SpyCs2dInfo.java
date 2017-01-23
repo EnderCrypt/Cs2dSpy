97eefe0777e058ad28c7ec83096231ba18fe4e39
Magnus <EnderCrypt@gmail.com>
2017-01-23 23:44:05 +0100

    added missing copyright headers

--- src/com/endercrypt/cs2dspy/representation/SpyCs2dInfo.java
+++ src/com/endercrypt/cs2dspy/representation/SpyCs2dInfo.java
@@ -5,6 +5,24 @@ import java.io.IOException;
 import com.endercrypt.cs2dspy.link.AccessSource;
 import com.endercrypt.cs2dspy.link.SpyAccess;
 
+/**
+ *	This file is part of Cs2dSpy and was created by Magnus Gunnarsson
+ *
+ *	Copyright (C) 2017  Magnus Gunnarsson (EnderCrypt)
+ *
+ *	This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 public class SpyCs2dInfo
 {
 	private static final SpyCs2dInfo instance = new SpyCs2dInfo();
